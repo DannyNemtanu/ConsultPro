9af58b38409590e5bb674e42e61b1a2ae1a967a9
Alibabas <danny.nemtanu@gmail.com>
2017-10-26 11:41:26 +0100

    Project Updated
    Footer Done

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,41 @@
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-</body>
+<!-- Footer -->
+<footer>
+  <div class="row container">
+      <div class="col-md-6">
+        <h3>SIGN UP TO <span id="bold">NEWSLETTER</span></h3>
+        <span><p>Keep up to date with lattest news & offer.s</p></span>
+        <input id="footer-newsletter" type="email" name="newsletter-email" value="" placeholder="email@example.com">
+      </div>
+      <div class="col-md-6">
+        <h3>FOLLOW <span id="bold">US</span></h3>
+        <div class="row">
+          <ul id="social-links">
+            <li><a href="#"><img src="../img/linkedin.png" alt="LinkedIn"></a></li>
+            <li><a href="#"><img src="../img/facebook.png" alt="Facebook"></a></li>
+            <li><a href="#"><img src="../img/instagram.png" alt="Instagram"></a></li>
+            <li><a href="#"><img src="../img/twitter.png" alt="Twitter"></a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="row footer-nav">
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">BLOG</a></li>
+          <li><a href="#">EXPLORE</a></li>
+          <li><a href="#">CONSULTANTS</a></li>
+        </ul>
+      </div>
+  </div>
+  <div class="row copyright">
+    <p>Published in 2017. Copyright ProConsult. All Right Reserved.</p>
+  </div>
+</footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+    <script src="js/app.js"></script>
+  </body>
 </html>
