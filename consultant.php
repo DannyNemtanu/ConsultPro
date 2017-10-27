d3ab39827196e22f19e07fc9d1728b4bcf58610b
Alibabas <danny.nemtanu@gmail.com>
2017-10-27 12:49:29 +0100

    Consultant Page
    Content Added to consultant page.
    Header must be changed.
    Footer must be added

--- consultant.php
+++ consultant.php
@@ -1,85 +1,198 @@
-<!-- Include Header -->
-<?php include 'inc/header.php'; ?>
-<!-- Including Navigation -->
-<?php  include 'inc/navigation.php'; ?>
-  <!-- Body Starts -->
-  <!-- Heder -->
-  <div class="jumbotron header-banner">
-    <div class="container-fluid">
-      <div class="row header-info">
-           <h1>FIND CONSULTANTS <br> IN MINUTES</h1>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation </p>
-           <button type="button" class="btn btn-primary header-inf-btn">EXPLORE NOW</button>
-      </div>
-      <div class="row header-form">
-        <form class="home-form" action="index.html" method="post">
-          <label for="username">Username</label><br>
-          <input type="text" name="username" placeholder="Username"><br>
-          <label for="username">Password</label><br>
-          <input type="password" name="pass" placeholder="Password">
-          <div class="form-footer">
-            <button type="button" class="btn btn-primary header-inf-btn">LOG IN</button> <br>
-            <span>Don't have an account yet?</span><br>
-            <span><a id="color-red" href="#">SIGN UP</a></span><br>
+<?php
+  include 'inc/header.php';
+  include 'inc/navigation.php';
+ ?>
+
+ <!-- header -->
+ <header>
+   <div class="consultant-banner">
+     <div class="row">
+       <h1>Welcome! <br> Name Surname</h1>
+       <p>Here you can see all you recent activity</p>
+       <img src="img/consultant.png" alt="">
+     </div>
+   </div>
+ </header>
+ <!-- end header -->
+<!-- content -->
+  <div class="consultant-content row col-xs-12">
+    <!-- Left Side -->
+    <div class="consultant-main col-md-8 col-xs-12">
+      <div class="row">
+        <div class="col-xs-12 section-content dashboard">
+          <!-- Consultant Dashboard -->
+          <div class="row text">
+            <h1 id="bold">Your Dashboard</h1>
+            <h4>Private to you !</h4>
           </div>
-        </form>
+          <div class="row dash-info">
+            <div class="col-md-4">
+              <h1>13</h1>
+              <h4>Who viewed you account</h4>
+            </div>
+            <div class="col-md-4">
+              <h1>0</h1>
+              <h4>Feedbacks to your account</h4>
+            </div>
+            <div class="col-md-4">
+              <h1>13</h1>
+              <h4>Search appearance</h4>
+            </div>
+          </div>
+        </div>
+        <!-- End Dashborrd -->
+
+        <!-- Expirience -->
+        <div class="row expirience">
+          <div class="col-xs-12 section-content">
+            <div class="col-md-3 hidden-xs">
+              <img src="img/consBuilding.png" alt="Building">
+            </div>
+            <div class="col-md-9 col-xs-12">
+              <h1>Job Title</h1>
+              <h4>Company Name</h4>
+              <p>2016 - Present</p>
+              <p>Location</p>
+
+              <p>
+                Responding to complaints applying tact, diplomacy and judgement.<br>
+                Maintaining well-built relationships with staff and members.<br>
+                Reconcile and balancing the transaction at the end of the shift.<br>
+                Perform credit checks and check credit references.<br>
+                Restock and replenish casino supplies.<br>
+              </p>
+            </div>
+          </div>
+        </div>
+        <!-- End Expirience -->
+
+        <!-- Education -->
+        <div class="row education">
+          <div class="row">
+            <div class="col-xs-12 section-content">
+              <div class="col-md-3 hidden-xs">
+                <img src="img/consEducation.png" alt="College Image">
+              </div>
+              <div class="col-md-9 col-xs-12">
+                <h1>National College of Ireland</h1>
+                <h4>Bachelor's Degree, BSc (Honours) in Accounting</h4>
+                <span>2017 - 2019</span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 section-content">
+              <div class="col-md-3 hidden-xs">
+                <img src="img/consEducation.png" alt="College Image">
+              </div>
+              <div class="col-md-9 col-xs-12">
+                <h1>National College of Ireland</h1>
+                <h4>Bachelor's Degree, Business Support</h4>
+                <span>2015 - 2017</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- End Education -->
+
+        <!-- Certificates -->
+        <div class="row certificates  ">
+          <div class="row">
+            <div class="col-xs-12 section-content">
+              <div class="col-md-3 hidden-xs">
+                <img src="img/consCertificate.png" alt="Certificate Image">
+              </div>
+              <div class="col-md-9 col-xs-12">
+                <h1>ACCA QUALIFICATION</h1>
+                <h4>PROFESSIONAL LEVEL CERTIFICATE</h4>
+                <span>2017 - 2019</span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 section-content">
+              <div class="col-md-3 hidden-xs">
+                <img src="img/consCertificate.png" alt="Certificate Image">
+              </div>
+              <div class="col-md-9 col-xs-12">
+                <h1>FOUNDATIONS IN ACCOUNTANCY</h1>
+                <h4>FOUNDATIONS IN AUDIT</h4>
+                <span>2015 - 2017</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- End Certificates -->
       </div>
+    </div>
+    <!-- Right Side -->
+    <div class="consultant-raling hidden-xs col-md-4">
+      <div class="row consultant-raling-padding">
+        <div class="row share">
+          <div class="col-md-12">
+            <div class="row">
+              <h3>Share with friends!</h3>
+              <div class="row">
+                <div class="col-md-4">
+                  <a href="#"><img src="img/facebookShare.png" alt=""></a>
+                </div>
+                <div class="col-md-4">
+                  <a href="#"><img src="img/linkedinShare.png" alt=""></a>
+                </div>
+                <div class="col-md-4">
+                  <a href="#"><img src="img/twitterShare.png" alt=""></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Suggestions -->
+        <div class="row suggestions">
+          <div class="col-md-12">
+            <h3>Our Suggested</h3>
+            <!-- Consultants List -->
+            <div class="row consultant-suugested">
+              <div class="row">
+                <div class="col-md-4 img">
+                  <img src="img/consultant.png" alt="">
+                </div>
+                <div class="col-md-8">
+                  <h4>Name Surname</h4>
+                  <h5>Accounting | Business | Qualified</h5>
+                  <h6>Rating: <br> 5+ Star</h6>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4 img">
+                  <img src="img/consultant.png" alt="">
+                </div>
+                <div class="col-md-8">
+                  <h4>Name Surname</h4>
+                  <h5>Accounting | Business | Qualified</h5>
+                  <h6>Rating: <br> 5+ Star</h6>
+                </div>
+              </div>
+              <div class="row no-border">
+                <div class="col-md-4 img">
+                  <img src="img/consultant.png" alt="">
+                </div>
+                <div class="col-md-8">
+                  <h4>Name Surname</h4>
+                  <h5>Accounting | Business | Qualified</h5>
+                  <h6>Rating: <br> 5+ Star</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <button type="button" class="btn btn-primary suggested-btn" name="button">See More</button>
+            </div>
+          </div>
+        </div>
+        <!-- End Suggestions -->
+      </div>
+
     </div>
   </div>
-
-    <!-- Content -->
-    <div class="content">
-      <div class="row">
-        <div class="col-sm-12 title">
-          <h1>PROFFESIONALS <span id="bold">IN</span></h1>
-        </div>
-      </div>
-      <div class="row container explanation">
-        <div class="col-md-4">
-          <img src="img/businessicon.png" alt="Business Icon">
-          <h3>BUSINESS</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip </p>
-        </div>
-        <div class="col-md-4">
-          <img src="img/managementIcon.png" alt="Managment Icon">
-          <h3>MANAGEMENT</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip </p>
-        </div>
-        <div class="col-md-4">
-          <img src="img/accountingIcon.png" alt="Accounting Icon">
-          <h3>ACCOUNTING</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip </p>
-        </div>
-      </div>
-      <div class="row container explanation">
-        <div class="col-md-4">
-          <img src="img/marketingIcon.png" alt="Business Icon">
-          <h3>MARKETING</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip </p>
-        </div>
-        <div class="col-md-4">
-          <img src="img/tutorialIcon.png" alt="Business Icon">
-          <h3>TUTORIALS</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip </p>
-        </div>
-        <div class="col-md-4">
-          <img src="img/eventIcon.png" alt="Business Icon">
-          <h3>EVENTS</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip </p>
-        </div>
-      </div>
-    </div>
-
-    <!-- Testimonials -->
-    <div class="row testimonials">
-      <div class="col-xs-offset-3 col-xs-6">
-        <h2><span id="bold" >OUR</span> TESTIMONIALS</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure </p>
-        <h4>by <span id="bold">Jonathan Lynch</span></h4>
-      </div>
-    </div>
-    <!-- Body Ends -->
-
-
-  <!-- Include Footer -->
-  <?php include 'inc/footer.php'; ?>
+<!-- end content -->
--- inc/navigation.php
+++ inc/navigation.php
@@ -26,7 +26,67 @@
             <li><a href="#">Log Out</a></li>
           </ul>
         </li>
+        <li><button href="#" type="button" class="btn btn-primary nav-sign" data-toggle="modal" data-target=".signup" >Sign Up</button></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
+
+<!-- Sign Up Modal Form -->
+<div class="modal fade bs-example-modal-sm signup" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+  <div class="modal-dialog modal-lg nav-sign-modal" role="document">
+    <div class="row">
+        <div class="col-xs-12 well well-sm nav-well">
+            <legend></a> Sign up!</legend>
+            <form action="#" method="post" class="form" role="form">
+            <div class="row">
+                <div class="col-xs-6 col-md-6">
+                    <input class="form-control" name="firstname" placeholder="First Name" type="text"
+                        required autofocus />
+                </div>
+                <div class="col-xs-6 col-md-6">
+                    <input class="form-control" name="lastname" placeholder="Last Name" type="text" required />
+                </div>
+            </div>
+            <input class="form-control" name="youremail" placeholder="Your Email" type="email" />
+            <input class="form-control" name="reenteremail" placeholder="Re-enter Email" type="email" />
+            <input class="form-control" name="password" placeholder="New Password" type="password" />
+            <input class="form-control" name="password" placeholder="Re-enter Password" type="password" />
+            <label for="">
+                Birth Date</label>
+            <div class="row">
+                <div class="col-xs-4 col-md-4">
+                    <select class="form-control">
+                        <option value="Month">Month</option>
+                    </select>
+                </div>
+                <div class="col-xs-4 col-md-4">
+                    <select class="form-control">
+                        <option value="Day">Day</option>
+                    </select>
+                </div>
+                <div class="col-xs-4 col-md-4">
+                    <select class="form-control">
+                        <option value="Year">Year</option>
+                    </select>
+                </div>
+            </div>
+            <!-- <label class="radio-inline">
+                <input type="radio" name="sex" id="inlineCheckbox1" value="male" />
+                Male
+            </label>
+            <label class="radio-inline">
+                <input type="radio" name="sex" id="inlineCheckbox2" value="female" />
+                Female
+            </label> -->
+            <br />
+            <br />
+            <div class="row btn-row">
+              <button class="btn btn-lg btn-primary  nav-form-btn" type="submit">New Customer</button>
+              <button class="btn btn-lg btn-primary  nav-form-btn" type="submit">New Consultant</button>
+            </div>
+            </form>
+        </div>
+    </div>
+    </div>
+</div>
