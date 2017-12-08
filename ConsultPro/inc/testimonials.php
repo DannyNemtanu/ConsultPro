ea67c3c7768dd273a3e040a66e829a80470bdf63
Daniel Hannigan <danielhannigan96@gmail.com>
2017-12-08 15:28:56 +0000

    Deleted Extras

--- ConsultPro/inc/testimonials.php
+++
@@ -1,9 +0,0 @@
-
-    <!-- Testimonials -->
-    <div class="row testimonials">
-      <div class="col-xs-offset-3 col-xs-6">
-        <h2><span id="bold" >OUR</span> TESTIMONIALS</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure </p>
-        <h4>by <span id="bold">Jonathan Lynch</span></h4>
-      </div>
-    </div>
