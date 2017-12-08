f3ec46694c4bb9559ee093f477d3902c07c35454
Daniel Hannigan <danielhannigan96@gmail.com>
2017-12-08 15:23:50 +0000

    Updated customer page
    Updated the dashboard on customer page with buttons that lead to new php pages.

---
+++ ConsultPro/feedbackpage.php
@@ -0,0 +1,181 @@
+<?php include 'inc/header.php'; ?>
+<?php include 'inc/customer-nav.php'; ?>
+
+ <!-- header -->
+ <!-- <header>
+   <div class="consultant-banner">
+     <div class="row container">
+       <div class="col-sm-4 col-sm-offset-4 cons-banner-left">
+         <h1>Welcome! <br> Name Surname</h1>
+         <p>Here you can see all you recent activity</p>
+         <img src="img/consultant.png" alt="">
+       </div>
+     </div>
+   </div>
+ </header> -->
+ <!-- end header -->
+<!-- content -->
+  <div class="customer-content row col-xs-12">
+    <!-- Left Side -->
+    <div class="consultant-main col-md-8 col-xs-12">
+      <div class="row">
+        <div class="col-xs-12 section-content dashboard">
+          <!-- Consultant Dashboard -->
+          <div class="row text">
+            <h1 id="bold">Your Dashboard</h1>
+            <h4>Private to you !</h4>
+          </div>
+          <div class="row dash-info">
+            <a href="likedconsultants.php">
+              <div class="col-md-4">
+                <h1>13</h1>
+                <h4>Liked Consultants</h4>
+              </div>
+            </a>
+              <a href="feedbackpage.php">
+                <div class="col-md-4">
+                  <h1>2</h1>
+                  <h4>Feedback Given</h4>
+                </div>
+                <a href="ratedconsultants.php">
+                  <div class="col-md-4">
+                    <h1>13</h1>
+                    <h4>Rated Consultants</h4>
+                  </div>
+          </div>
+        </div>
+        </div>
+        <!-- End Dashborrd -->
+
+
+      <!-- Recomended Consultants -->
+      <div class="container">
+      <div class="row">
+      <div class="col-sm-12">
+      <h3>Consultant Feedback Given</h3>
+      </div><!-- /col-sm-12 -->
+      </div><!-- /row -->
+      <div class="row">
+      <div class="col-sm-1">
+      <div class="thumbnail">
+      <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">
+      </div><!-- /thumbnail -->
+      </div><!-- /col-sm-1 -->
+
+      <div class="col-sm-5">
+      <div class="panel panel-default">
+      <div class="panel-heading">
+      <strong>myusername</strong> <span class="text-muted">commented 5 days ago</span>
+      </div>
+      <div class="panel-body">
+      Panel content
+      </div><!-- /panel-body -->
+      </div><!-- /panel panel-default -->
+      </div><!-- /col-sm-5 -->
+
+      <div class="col-sm-1">
+      <div class="thumbnail">
+      <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">
+      </div><!-- /thumbnail -->
+      </div><!-- /col-sm-1 -->
+
+      <div class="col-sm-5">
+      <div class="panel panel-default">
+      <div class="panel-heading">
+      <strong>myusername</strong> <span class="text-muted">commented 5 days ago</span>
+      </div>
+      <div class="panel-body">
+      Panel content
+      </div><!-- /panel-body -->
+      </div><!-- /panel panel-default -->
+      </div><!-- /col-sm-5 -->
+      </div><!-- /row -->
+
+      </div><!-- /container -->
+
+
+      <!-- Recomended Consultants -->
+    </div>
+    <!-- Right Side -->
+    <div class="consultant-raling hidden-xs col-md-4">
+      <div class="row consultant-raling-padding">
+        <div class="row share">
+          <div class="col-md-12">
+            <div class="row">
+              <h3>Share your profile with friends!</h3>
+              <div class="row">
+                <div class="col-md-4">
+                  <a href="#"><img src="img/facebookShare.png" alt=""></a>
+                </div>
+                <div class="col-md-4">
+                  <a href="#"><img src="img/linkedinShare.png" alt=""></a>
+                </div>
+                <div class="col-md-4">
+                  <a href="#"><img src="img/twitterShare.png" alt=""></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Suggestions -->
+        <div class="row suggestions">
+          <div class="col-md-12">
+            <h3>Recommended Consultants</h3>
+            <!-- Conntected Consultants List -->
+            <div class="row consultant-suugested">
+              <div class="row">
+                <div class="col-md-4 img">
+                  <img src="img/consultant.png" alt="">
+                </div>
+                <div class="col-md-8">
+                  <h4>Name Surname</h4>
+                  <h5>Accounting | Business | Qualified</h5>
+                  <h6>Rating: <br> 5+ Star</h6>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4 img">
+                  <img src="img/consultant.png" alt="">
+                </div>
+                <div class="col-md-8">
+                  <h4>Name Surname</h4>
+                  <h5>Accounting | Business | Qualified</h5>
+                  <h6>Rating: <br> 5+ Star</h6>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4 img">
+                  <img src="img/consultant.png" alt="">
+                </div>
+                <div class="col-md-8">
+                  <h4>Name Surname</h4>
+                  <h5>Accounting | Business | Qualified</h5>
+                  <h6>Rating: <br> 5+ Star</h6>
+                </div>
+              </div>
+              <div class="row no-border">
+                <div class="col-md-4 img">
+                  <img src="img/consultant.png" alt="">
+                </div>
+                <div class="col-md-8">
+                  <h4>Name Surname</h4>
+                  <h5>Accounting | Business | Qualified</h5>
+                  <h6>Rating: <br> 5+ Star</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <button type="button" class="btn btn-primary suggested-btn" name="button">See More</button>
+            </div>
+          </div>
+        </div>
+        <!-- Connected Customers -->
+      </div>
+    </div>
+  </div>
+
+<!-- end content -->
+
+<?php include 'inc/footer.php'; ?>
---
+++ ConsultPro/inc/navigation.php
@@ -0,0 +1,214 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="consultant.php">Explore</a></li>
+        <li><button href="#" type="button" class="btn btn-primary nav-sign" data-toggle="modal" data-target=".signup" >Sign Up</button></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<!-- Sign Up Modal Form -->
+<div class="modal fade signup-modal bs-example-modal-sm signup" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+  <div class="modal-dialog modal-lg nav-sign-modal" role="document">
+      <form method="post" action="index.php" autocomplete="on" id="signup-form">
+        <div id="login-box">
+          <div class="left">
+            <h1>Sign up</h1>
+            <p style="color:red" id="signup-error"></p>
+            <input type="text" id="registerEmail" name="email" placeholder="E-mail"/>
+            <input type="password" id="registerPass" name="pass" placeholder="Password"/>
+            <input type="password" id="registerCPass" name="cpass" placeholder="Retype password"/>
+            <div class="row">
+              <div class="col-md-6">
+                <input id="customer-signup" type="submit" name="signup_submit" value="Customer" />
+              </div>
+              <div class="col-md-6">
+                <input id="consultant-signup" type="submit" name="signup_submit" value="Consultant" />
+              </div>
+            </div>
+
+          </div>
+
+          <div class="right">
+            <span class="loginwith">Sign in with<br />social network</span>
+
+            <button class="social-signin facebook">Log in with facebook</button>
+            <button class="social-signin twitter">Log in with Twitter</button>
+            <button class="social-signin google">Log in with Google+</button>
+          </div>
+          <div class="or">OR</div>
+        </div>
+      </form>
+    </div>
+</div>
+
+<style media="screen">
+#login-box input:focus {
+outline: none;
+}
+
+#login-box {
+position: relative;
+margin: 5% auto;
+width: 600px;
+height: 400px;
+background: #FFF;
+border-radius: 2px;
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+}
+
+#login-box .left {
+position: absolute;
+top: 0;
+left: 0;
+box-sizing: border-box;
+padding: 40px;
+width: 300px;
+height: 400px;
+}
+#login-box #left-first-child{
+  margin-top: 50px;
+}
+
+#login-box h1 {
+margin: 0 0 20px 0;
+font-weight: 300;
+font-size: 28px;
+}
+
+#login-box input[type="text"],
+#login-box input[type="password"] {
+display: block;
+box-sizing: border-box;
+margin-bottom: 20px;
+padding: 4px;
+width: 220px;
+height: 32px;
+border: none;
+border-bottom: 1px solid #AAA;
+font-family: 'Roboto', sans-serif;
+font-weight: 400;
+font-size: 15px;
+transition: 0.2s ease;
+}
+
+#login-box input[type="text"]:focus,
+#login-box input[type="password"]:focus {
+border-bottom: 2px solid #16a085;
+color: #16a085;
+transition: 0.2s ease;
+}
+
+#login-box input[type="submit"] {
+margin-left: -10px;
+margin-top: 28px;
+padding: 10px;
+background: #16a085;
+border: none;
+border-radius: 2px;
+color: #FFF;
+font-family: 'Roboto', sans-serif;
+font-weight: 500;
+text-transform: uppercase;
+transition: 0.1s ease;
+cursor: pointer;
+}
+
+#login-box input[type="submit"]:hover,
+#login-box input[type="submit"]:focus {
+opacity: 0.8;
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+transition: 0.1s ease;
+}
+
+#login-box input[type="submit"]:active {
+opacity: 1;
+box-shadow: 0 1px 2px rgba(0, 0, 0, 0.4);
+transition: 0.1s ease;
+}
+
+#login-box .or {
+position: absolute;
+top: 180px;
+left: 280px;
+width: 40px;
+height: 40px;
+background: #DDD;
+border-radius: 50%;
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+line-height: 40px;
+text-align: center;
+}
+
+#login-box .right {
+position: absolute;
+top: 0;
+right: 0;
+box-sizing: border-box;
+padding: 40px;
+width: 300px;
+height: 400px;
+background: url('https://cdn.pixabay.com/photo/2015/02/02/11/08/office-620817_1280.jpg');
+background-size: cover;
+background-position: center;
+border-radius: 0 2px 2px 0;
+}
+
+#login-box .right .loginwith {
+display: block;
+margin-bottom: 40px;
+font-size: 28px;
+color: #FFF;
+text-align: center;
+}
+
+#login-box button.social-signin {
+margin-bottom: 20px;
+width: 220px;
+height: 36px;
+border: none;
+border-radius: 2px;
+color: #FFF;
+font-family: 'Roboto', sans-serif;
+font-weight: 500;
+transition: 0.2s ease;
+cursor: pointer;
+}
+
+#login-box button.social-signin:hover,
+#login-box button.social-signin:focus {
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+transition: 0.2s ease;
+}
+
+#login-box button.social-signin:active {
+box-shadow: 0 1px 2px rgba(0, 0, 0, 0.4);
+transition: 0.2s ease;
+}
+
+#login-box button.social-signin.facebook {
+background: #32508E;
+}
+
+#login-box button.social-signin.twitter {
+background: #55ACEE;
+}
+
+#login-box button.social-signin.google {
+background: #DD4B39;
+}
+</style>
