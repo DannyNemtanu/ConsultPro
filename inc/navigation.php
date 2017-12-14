70c005ec9ede26fb94f3077ef185249b90fd3758
Alibabas <danny.nemtanu@gmail.com>
2017-12-14 13:24:28 +0000

    Links Update
    Links Update

--- inc/navigation.php
+++ inc/navigation.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.php">Brand</a>
+      <a class="navbar-brand" href="/consultantPro/index.php">eKonsult</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
